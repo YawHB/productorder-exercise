38a0c073b0572902d2cec52387ed95d2df35d274
YawHB <yawhboateng@gmail.com>
2024-02-16 10:37:55 +0100

    init commit

---
+++ src/main/java/kea/exercises/productorderexercise/ProductorderExerciseApplication.java
@@ -0,0 +1,13 @@
+package kea.exercises.productorderexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductorderExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductorderExerciseApplication.class, args);
+    }
+
+}
